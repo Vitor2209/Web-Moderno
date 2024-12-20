9ca78acb9e5806c4b1bdab5d96079a16014e2bce
Vitor2209 <vitordutra1123@gmail.com>
2024-12-20 18:58:56 -0300

    começando um novo curso de web moderno, e esses são os primeiros exercícios de fixação

---
+++ Exercicio-JS/Fundamentos/organizacao.js
@@ -0,0 +1,14 @@
+console.log('Sentença de código');
+
+
+{
+
+    {
+        console.log("Olá");
+        console.log("Mundo!"); //Padrão do curso 
+    }
+    
+}
+
+
+console.log(1 + 2);
---
+++ Exercicio-JS/comentario.js
@@ -0,0 +1,8 @@
+// Comentário de uma linha
+console.log('Linha 1');
+
+/*
+Comentáario de
+múltiplas linhas
+*/ 
+console.log('Linha 3');
